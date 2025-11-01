446ea8d4697ea6266d73c842b6cc9ee0e1bc8f30
Fenet Gizaw <simbani011@gmail.com>
2025-11-01 13:45:45 -0700

    Update utils.py

--- analysis/app/utils.py
+++ analysis/app/utils.py
@@ -10,6 +10,7 @@ nltk.download('stopwords', quiet=True)
 def preprocess_text(text):
     text = text.lower()
     text = re.sub(r'[^a-z0-9\s]', '', text)
+
     words = text.split()
     stop_words = set(stopwords.words('english'))
     words = [word for word in words if word not in stop_words]
