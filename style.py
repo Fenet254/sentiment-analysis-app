3d9202bf27341cba5cb3f95eeaf2d45c44f7b193
Fenet Gizaw <simbani011@gmail.com>
2026-02-16 22:57:30 +0300

    Update style.py

--- style.py
+++ style.py
@@ -1,4 +1,4 @@
-ggvzvgtzzz6bbgzhhguuugghhgg1¹1¹bbvtgzzhgtzghzzdgtghsgsuzzzgcgvhgggghcaggggzgggzhhh xccdcfgvhxefdftg
+gggvzvgtzzz6bbgzhhguuugghhgg1¹1¹bbvtgzzhgtzghzzdgtghsgsuzzzgcgvhgggghcaggggzgggzhhh xccdcfgvhxefdftg
 * {
     margin: 0;
     padding: 0;h
